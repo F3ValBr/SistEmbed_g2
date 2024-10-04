c2a956bd6a262fc1bf22b04bdec3d5b198af7431
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2024-10-04 14:13:55 -0300

    feat(esp): implementa envío de mensaje con espera de ACK

--- esp/include/embebidos/uart.h
+++ esp/include/embebidos/uart.h
@@ -7,8 +7,24 @@
 #define RXD_PIN 3            // UART RX pin
 #define UART_NUM UART_NUM_0  // UART port number
 #define BAUD_RATE 115200     // Baud rate
+#define WRITE_DELAY_MS 1000  // Tiempo de espera post escritura
 
 int uart1_printf(const char *str, va_list ap);
 void uart_setup();
 int serial_read(char *buffer, int size);
 int serial_write_0(const char *msg, int len);
+
+typedef enum msg_ack {
+    OK,       // No hay problema con el mensaje.
+    CORRUPT,  // Problema con largo de mensaje o checksum.
+    INVALID   // Paquete íntegro pero con datos no válidos.
+} msg_ack;
+
+/**
+ * @brief Envía un mensaje esperando por confirmación
+ *
+ * @param msg Puntero al mensaje.
+ * @param msg_size Tamaño del mensaje.
+ * @return msg_ack ACK recibido.
+ */
+msg_ack send_n_confirm(char *buffer, int msg_size);
--- esp/main/uart.c
+++ esp/main/uart.c
@@ -2,6 +2,7 @@
 
 #include <string.h>
 
+#include "embebidos/checksum.h"
 #include "esp_log.h"
 
 // Function for sending things to UART1
@@ -52,4 +53,108 @@ int serial_write_0(const char *msg, int len) {
 
     vTaskDelay(pdMS_TO_TICKS(1000));  // Delay for 1 second
     return result;
+}
+
+/**
+ * @brief Envía un mensaje por UART e inmediatamente invoca un delay según la
+ * definición en header.
+ *
+ * @param data Datos a enviar.
+ * @param size Tamaño de paquete a enviar.
+ */
+void uart_write_delay(void *data, size_t size) {
+    uart_write_bytes(UART_NUM, (const char *)data, size);
+    vTaskDelay(pdMS_TO_TICKS(WRITE_DELAY_MS));
+}
+
+void write_pkg_init(char *pkg_start) {
+    memcpy(pkg_start, "ABCD", 4);
+}
+
+void write_pkg_end(char *pkg_end) {
+    memcpy(pkg_end, "DCBA", 4);
+}
+
+int get_pkg_end_index(int pkg_size) {
+    int end_index = pkg_size - 4;
+    return end_index;
+}
+
+int get_pkg_len(int msg_size) {
+    // INICIO + CHECKSUM + LARGO + msg_size + FIN
+    int pkg_len = sizeof(char) * 4 + sizeof(unsigned int) +
+                  sizeof(unsigned int) + msg_size + sizeof(char) * 4;
+    return pkg_len;
+}
+
+/**
+ * @brief Crea un paquete con INICIO de paquete, CHECKSUM de mensaje, LARGO de
+ * mensaje, MENSAJE y FIN de paquete.
+ *
+ * @param pkg Puntero al paquete final.
+ * @param buffer Puntero al mensaje a empaquetar.
+ * @param size Tamaño del mensaje.
+ */
+void wrap_msg(char *pkg, char *buffer, int msg_size) {
+    char *pkg_index = pkg;  // Cursor en posición a escribir.
+
+    // Escribe inicio
+    write_pkg_init(pkg);
+    pkg_index += 4;
+
+    // Escribe checksum
+    unsigned int chksm = checksum(buffer);
+    memcpy(pkg_index, &chksm, sizeof(unsigned int));
+    pkg_index += sizeof(unsigned int);
+
+    // Escribe largo de mensaje
+    memcpy(pkg_index, &msg_size, sizeof(unsigned int));
+    pkg_index += sizeof(unsigned int);
+
+    // Escribe mensaje
+    memcpy(pkg_index, buffer, msg_size);
+    pkg_index += msg_size;
+
+    // Escribe final
+    write_pkg_end(pkg_index);
+}
+
+/**
+ * @brief Envoltura para obtener tamaño del buffer con pausas en caso de haber
+ * error.
+ *
+ * @param size Tamaño de buffer.
+ */
+void check_buf_len(size_t *size) {
+    while (uart_get_buffered_data_len(UART_NUM, size) == ESP_FAIL) {
+        vTaskDelay(pdMS_TO_TICKS(WRITE_DELAY_MS));
+    }
+}
+
+/**
+ * @brief Espera a haber un único byte en el buffer para extraer el ACK.
+ *
+ * @return msg_ack ACK extraído.
+ */
+msg_ack read_ack() {
+    size_t buf_len;
+    do {
+        check_buf_len(&buf_len);
+    } while (buf_len != 1);
+    char ack;
+    serial_read(&ack, sizeof(ack));
+    return (msg_ack)ack;
+}
+
+msg_ack send_n_confirm(char *msg, int msg_size) {
+    int pkg_len = get_pkg_len(msg_size);
+    char pkg[pkg_len];
+    wrap_msg(pkg, msg, msg_size);
+    uart_write_delay(pkg, pkg_len);
+    msg_ack ack = read_ack();
+    while (ack == CORRUPT) {
+        uart_write_delay(pkg, pkg_len);
+        ack = read_ack();
+    }
+    return ack;
 }
