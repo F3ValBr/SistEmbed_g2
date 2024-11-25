15a859e1bbc67802ec273e559895d9c500505153
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2024-11-24 22:30:21 -0300

    feat: adapta cliente a bmi

--- cliente/controller.py
+++ cliente/controller.py
@@ -1,4 +1,4 @@
-from struct import unpack, pack
+from struct import unpack, pack, calcsize
 from serial import Serial
 from time import sleep
 import numpy as np
@@ -11,22 +11,32 @@ class MissingWindowSizeError(Exception):
     pass
 
 
+READING_VALUE_QTY = 6
+
+
 class Controller:
     """Clase que agrupa métodos para el flujo de datos."""
     running = True
     window_data = []
-    temp_top = None
-    pres_top = None
-    hum_top = None
-    gas_top = None
-    temp_rms = None
-    pres_rms = None
-    hum_rms = None
-    gas_rms = None
-    temp_fft = []
-    pres_fft = []
-    hum_fft = []
-    gas_fft = []
+    # TODO: reemplazar campos de gyro + acc
+    acc_x_top = None
+    acc_y_top = None
+    acc_z_top = None
+    gyro_x_top = None
+    gyro_y_top = None
+    gyro_z_top = None
+    acc_x_rms = None
+    acc_y_rms = None
+    acc_z_rms = None
+    gyro_x_rms = None
+    gyro_y_rms = None
+    gyro_z_rms = None
+    acc_x_fft = []
+    acc_y_fft = []
+    acc_z_fft = []
+    gyro_x_fft = []
+    gyro_y_fft = []
+    gyro_z_fft = []
 
     def __init__(self, serial_port=None):
         self.window_size = 10
@@ -44,14 +54,14 @@ class Controller:
         self.send_message(self.pack_signal(0))
         self.window_data = []
 
-    def receive_bme_data(self):
-        data = self.ser.read(16)
-        data = unpack("ffff", data)
+    def receive_bmi_data(self):
+        data = self.ser.read(READING_VALUE_QTY * calcsize("f"))
+        data = unpack(f"{READING_VALUE_QTY}f", data)
         return data
 
     def receive_fft_pack(self):
-        data = self.ser.read(32)
-        data = unpack("8f", data)
+        data = self.ser.read(READING_VALUE_QTY * 2 * calcsize("f"))
+        data = unpack(f"{READING_VALUE_QTY*2}f", data)
         return data
 
     def receive_top(self):
@@ -59,19 +69,20 @@ class Controller:
         data = unpack("5f", data)
         return data
 
-    def receive_rms(self):
-        data = self.ser.read(12)
-        data = unpack("fff", data)
-        return data
+    # TODO: remover sin usar
+    # def receive_rms(self):
+    #     data = self.ser.read(12)
+    #     data = unpack("fff", data)
+    #     return data
 
     def get_window(self):
         print("Extrayendo ventana...")
         window_received = False
         win_response = None
         while True:
-            if self.ser.in_waiting > 7:
+            if self.ser.in_waiting > calcsize('Q') - 1:
                 try:
-                    win_response = self.ser.read(8)
+                    win_response = self.ser.read(calcsize('Q'))
                     win_response = unpack("Q", win_response)[0]
                 except Exception as e:
                     print(e)
@@ -95,23 +106,14 @@ class Controller:
         message = pack("6s", "BEGIN\0".encode())
         self.send_message(message)
 
-        # recibir datos de la bme - mediciones
+        # recibir datos de la bmi - mediciones
         counter = 0
         while True:
-            if self.ser.in_waiting > 15:
+            if self.ser.in_waiting > READING_VALUE_QTY * 4 - 1:
                 try:
-                    obt_data = self.receive_bme_data()
+                    obt_data = self.receive_bmi_data()
                     print(obt_data)
-                    # self.window_data.append(obt_data)
-                    # Verifica si el primer elemento (temperatura) es válido
-                    temperatura = obt_data[0]  # Primer elemento de la tupla
-                    if (-50.0 <= temperatura <= 100.0 and
-                            abs(temperatura) > 0.001):
-                        self.window_data.append(obt_data)
-                    else:
-                        # Si no es válida, no se agrega y sigue esperando
-                        print(f"Temperatura fuera de rango: {temperatura}")
-                        counter -= 1
+                    self.window_data.append(obt_data)
                 except Exception as e:
                     print(e)
                     continue
@@ -132,7 +134,7 @@ class Controller:
         top5_array = []
 
         while True:
-            if self.ser.in_waiting > 19:
+            if self.ser.in_waiting > calcsize("f") * 5 - 1:
                 try:
                     top_data = self.receive_top()
                     print(top_data)
@@ -144,7 +146,7 @@ class Controller:
                     counter_measures += 1
                     print(f"Contador data: {counter_measures}")
                 finally:
-                    if counter_measures == 4:
+                    if counter_measures == READING_VALUE_QTY:
                         print("Medidas del top 5 obtenidas")
                         self.window_data.append(top5_array)
                         break
@@ -158,9 +160,9 @@ class Controller:
         counter_rms = 0
 
         while True:
-            if self.ser.in_waiting > 15:
+            if self.ser.in_waiting > READING_VALUE_QTY * calcsize("f") - 1:
                 try:
-                    rms_data = self.receive_bme_data()
+                    rms_data = self.receive_bmi_data()
                     print(rms_data)
                     self.window_data.append(rms_data)
                 except Exception as e:
@@ -174,19 +176,24 @@ class Controller:
                         print("Medidas RMS obtenidas")
                         break
         counter_fft = 0
-        self.temp_fft = []
-        self.pres_fft = []
-        self.hum_fft = []
-        self.gas_fft = []
+        # TODO: reemplazar arreglos de fft por gyro y acc
+        self.acc_x_fft = []
+        self.acc_y_fft = []
+        self.acc_z_fft = []
+        self.gyro_x_fft = []
+        self.gyro_y_fft = []
+        self.gyro_z_fft = []
         while True:
-            if self.ser.in_waiting > 31:
+            if self.ser.in_waiting > READING_VALUE_QTY * 2 * calcsize("f") - 1:
                 try:
                     fft_data = self.receive_fft_pack()
                     # self.window_data.append(fft_data)
-                    self.temp_fft.append((fft_data[0], fft_data[1]))
-                    self.pres_fft.append((fft_data[2], fft_data[3]))
-                    self.hum_fft.append((fft_data[4], fft_data[5]))
-                    self.gas_fft.append((fft_data[6], fft_data[7]))
+                    self.acc_x_fft.append((fft_data[0], fft_data[1]))
+                    self.acc_y_fft.append((fft_data[2], fft_data[3]))
+                    self.acc_z_fft.append((fft_data[4], fft_data[5]))
+                    self.gyro_x_fft.append((fft_data[6], fft_data[7]))
+                    self.gyro_y_fft.append((fft_data[8], fft_data[9]))
+                    self.gyro_z_fft.append((fft_data[10], fft_data[11]))
                 except Exception as e:
                     print(e)
                     continue
@@ -232,99 +239,142 @@ class Controller:
         return signal_type.to_bytes(1, "little") + body.to_bytes(4, "little")
 
     def plot_window(self):
-        data_bme = self.window_data[0:self.window_size]
+        data_bmi = self.window_data[0:self.window_size]
         data_top = self.window_data[-2]
         data_rms = self.window_data[-1]
-        self.temp_top = data_top[0]
-        self.pres_top = data_top[1]
-        self.hum_top = data_top[2]
-        self.gas_top = data_top[3]
-        self.temp_rms = data_rms[0]
-        self.pres_rms = data_rms[1]
-        self.hum_rms = data_rms[2]
-        self.gas_rms = data_rms[3]
 
-        data_array = np.array(data_bme)
-        data_temp = data_array[:, 0]
-        data_pres = data_array[:, 1]
-        data_hum = data_array[:, 2]
-        data_gas = data_array[:, 3]
+        self.acc_x_top = data_top[0]
+        self.acc_y_top = data_top[1]
+        self.acc_z_top = data_top[2]
+        self.gyro_x_top = data_top[3]
+        self.gyro_y_top = data_top[4]
+        self.gyro_z_top = data_top[5]
 
-        temp_points = []
-        pres_points = []
-        hum_points = []
-        gas_points = []
+        self.acc_x_rms = data_rms[0]
+        self.acc_y_rms = data_rms[1]
+        self.acc_z_rms = data_rms[2]
+        self.gyro_x_rms = data_rms[3]
+        self.gyro_y_rms = data_rms[4]
+        self.gyro_z_rms = data_rms[5]
+
+        data_array = np.array(data_bmi)
+        data_acc_x = data_array[:, 0]
+        data_acc_y = data_array[:, 1]
+        data_acc_z = data_array[:, 2]
+        data_gyro_x = data_array[:, 3]
+        data_gyro_y = data_array[:, 4]
+        data_gyro_z = data_array[:, 5]
+
+        acc_x_points = []
+        acc_y_points = []
+        acc_z_points = []
+        gyro_x_points = []
+        gyro_y_points = []
+        gyro_z_points = []
 
         for i in range(5):
-            temp_idx = np.where(data_temp == self.temp_top[i])
-            temp_points.append((temp_idx, self.temp_top[i]))
+            acc_x_idx = np.where(data_acc_x == self.acc_x_top[i])
+            acc_x_points.append((acc_x_idx, self.acc_x_top[i]))
 
-            pres_idx = np.where(data_pres == self.pres_top[i])
-            pres_points.append((pres_idx, self.pres_top[i]))
+            acc_y_idx = np.where(data_acc_y == self.acc_y_top[i])
+            acc_y_points.append((acc_y_idx, self.acc_y_top[i]))
 
-            hum_idx = np.where(data_hum == self.hum_top[i])
-            hum_points.append((hum_idx, self.hum_top[i]))
+            acc_z_idx = np.where(data_acc_z == self.acc_z_top[i])
+            acc_z_points.append((acc_z_idx, self.acc_z_top[i]))
 
-            gas_idx = np.where(data_gas == self.gas_top[i])
-            gas_points.append((gas_idx, self.gas_top[i]))
+            gyro_x_idx = np.where(data_gyro_x == self.gyro_x_top[i])
+            gyro_x_points.append((gyro_x_idx, self.gyro_x_top[i]))
 
-        temp_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.temp_fft]
-        pres_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.pres_fft]
-        hum_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.hum_fft]
-        gas_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.gas_fft]
+            gyro_y_idx = np.where(data_gyro_y == self.gyro_y_top[i])
+            gyro_y_points.append((gyro_y_idx, self.gyro_y_top[i]))
 
-        fig, axs = plt.subplots(2, 4)
+            gyro_z_idx = np.where(data_gyro_z == self.gyro_z_top[i])
+            gyro_z_points.append((gyro_z_idx, self.gyro_z_top[i]))
+
+        acc_x_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.acc_x_fft]
+        acc_y_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.acc_y_fft]
+        acc_z_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.acc_z_fft]
+        gyro_x_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.gyro_x_fft]
+        gyro_y_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.gyro_y_fft]
+        gyro_z_fft_vals = [np.sqrt(x[0]**2 + x[1]**2) for x in self.gyro_z_fft]
+
+        fig, axs = plt.subplots(2, READING_VALUE_QTY)
 
         fig.suptitle(
-            "Temperatura, Presion, Humedad y Gas" +
+            "Coordenadas Giroscopio y Acelerómetro" +
             f"(ventana: {self.window_size}")
 
-        axs[0, 0].plot(data_temp)
-        axs[0, 0].set_title(f"Temperatura (RMS = {self.temp_rms})")
-        axs[0, 0].set_ylabel("Grados Celsius (°C)")
-        axs[0, 0].set_ylim(0, 50)
+        # TODO: verificar limites de valores
+        axs[0, 0].plot(data_acc_x)
+        axs[0, 0].set_title(f"Coord x - Acelerómetro (RMS = {self.acc_x_rms})")
+        axs[0, 0].set_ylabel("x")
+        # axs[0, 0].set_ylim(0, 50)
 
-        for value in temp_points:
+        for value in acc_x_points:
             axs[0, 0].plot(value[0], value[1], "ro")
 
-        axs[0, 1].plot(data_pres)
-        axs[0, 1].set_title(f"Presion (RMS = {self.pres_rms})")
-        axs[0, 1].set_ylabel("Pascales (Pa)")
-        axs[0, 1].set_ylim(1000, 1500)
+        axs[0, 1].plot(data_acc_y)
+        axs[0, 1].set_title(f"Coord y - Acelerómetro (RMS = {self.acc_y_rms})")
+        axs[0, 1].set_ylabel("y")
+        # axs[0, 1].set_ylim(1000, 1500)
 
-        for value in pres_points:
+        for value in acc_y_points:
             axs[0, 1].plot(value[0], value[1], "ro")
 
-        axs[0, 2].plot(data_hum)
-        axs[0, 2].set_title(f"Humedad (RMS = {self.hum_rms})")
-        axs[0, 2].set_ylabel("Porcentaje (%)")
-        axs[0, 2].set_ylim(0, 100)
+        axs[0, 2].plot(data_acc_z)
+        axs[0, 2].set_title(f"Coord z - Acelerómetro (RMS = {self.acc_z_rms})")
+        axs[0, 2].set_ylabel("z")
+        # axs[0, 2].set_ylim(0, 100)
 
-        for value in hum_points:
+        for value in acc_z_points:
             axs[0, 2].plot(value[0], value[1], "ro")
 
-        axs[0, 3].plot(data_gas)
-        axs[0, 3].set_title(f"Gas (RMS = {self.gas_rms})")
-        axs[0, 3].set_ylabel("Ohmios (Ω)")
-        axs[0, 3].set_ylim(0, 100)
+        axs[0, 3].plot(data_gyro_x)
+        axs[0, 3].set_title(f"Coord x - Giroscópio (RMS = {self.gyro_x_rms})")
+        axs[0, 3].set_ylabel("x")
+        # axs[0, 3].set_ylim(0, 100)
 
-        for value in gas_points:
+        for value in gyro_x_points:
             axs[0, 3].plot(value[0], value[1], "ro")
 
-        axs[1, 0].plot(temp_fft_vals)
-        axs[1, 0].set_title("FFT Temperatura")
+        axs[0, 4].plot(data_gyro_y)
+        axs[0, 4].set_title(f"Coord y - Giroscópio (RMS = {self.gyro_y_rms})")
+        axs[0, 4].set_ylabel("y")
+        # axs[0, 3].set_ylim(0, 100)
+
+        for value in gyro_y_points:
+            axs[0, 4].plot(value[0], value[1], "ro")
+
+        axs[0, 5].plot(data_gyro_z)
+        axs[0, 5].set_title(f"Coord z - Giroscópio (RMS = {self.gyro_z_rms})")
+        axs[0, 5].set_ylabel("x")
+        # axs[0, 3].set_ylim(0, 100)
+
+        for value in gyro_z_points:
+            axs[0, 5].plot(value[0], value[1], "ro")
+
+        axs[1, 0].plot(acc_x_fft_vals)
+        axs[1, 0].set_title("FFT Coord x - Acelerómetro")
         axs[1, 0].set_ylabel("Amplitud")
 
-        axs[1, 1].plot(pres_fft_vals)
-        axs[1, 1].set_title("FFT Presion")
+        axs[1, 1].plot(acc_y_fft_vals)
+        axs[1, 1].set_title("FFT Coord y - Acelerómetro")
         axs[1, 1].set_ylabel("Amplitud")
 
-        axs[1, 2].plot(hum_fft_vals)
-        axs[1, 2].set_title("FFT Humedad")
+        axs[1, 2].plot(acc_z_fft_vals)
+        axs[1, 2].set_title("FFT Coord z - Acelerómetro")
         axs[1, 2].set_ylabel("Amplitud")
 
-        axs[1, 3].plot(gas_fft_vals)
-        axs[1, 3].set_title("FFT Gas")
+        axs[1, 3].plot(gyro_x_fft_vals)
+        axs[1, 3].set_title("FFT Coord x - Giroscópio")
         axs[1, 3].set_ylabel("Amplitud")
+
+        axs[1, 4].plot(gyro_y_fft_vals)
+        axs[1, 4].set_title("FFT Coord y - Giroscópio")
+        axs[1, 4].set_ylabel("Amplitud")
+
+        axs[1, 5].plot(gyro_z_fft_vals)
+        axs[1, 5].set_title("FFT Coord z - Giroscópio")
+        axs[1, 5].set_ylabel("Amplitud")
 
         plt.show()
